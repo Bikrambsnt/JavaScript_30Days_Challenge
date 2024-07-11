8c7f2930fa1b99ab457de3c4438be36f0a33b6f9
Bikrambsnt <bikrambasnett7@gmail.com>
2024-07-11 19:30:54 +0530

    Create how_js_execute_code.js

---
+++ how_js_execute_code.js
@@ -0,0 +1,6 @@
+//How Js will execute code is an intresting topic and 
+//I have try to make it easy using drawing and hope everyone will understand it
+
+  //To get the note please follow this link
+     
+   src="https://excalidraw.com/#json=itIvR3miab9jiNMrEAjFA,e-lcPCwY0QKGlanvPy2uaA"
