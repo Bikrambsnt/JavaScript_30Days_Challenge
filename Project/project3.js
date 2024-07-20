3e289042017c89c3bfd7ad43a5e71d5d0ff3e07e
Bikrambsnt <bikrambasnett7@gmail.com>
2024-07-20 20:54:26 +0530

    Update project3.js

--- Project/project3.js
+++ Project/project3.js
@@ -5,5 +5,6 @@ function showTime (){
     // console.log(date.toLocaleTimeString());
 clock.innerHTML =date.toLocaleTimeString();
 }
-setInterval (showTime,1000)
+setInterval (showTime,1000) //This is a function which execute code at every time ... it takes 2 call back and a delay time..
+
     showTime();
